d25e4571ab9adc1b62ecd6dcb1070707882183cd
Ishani <ishanit.0801@gmail.com>
2021-08-23 03:58:33 +0530

    added filter functionality

---
+++ src/common/header/Header.jsx
@@ -0,0 +1,30 @@
+import './Header.css';
+import React, {Component} from 'react';
+import logo from './../logo.svg';
+import Button from '@material-ui/core/Button';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          login: true
+        };
+        this.changeState = this.changeState.bind(this);
+      }
+      changeState = () => {
+        this.setState({
+          login: !this.state.login
+        });
+      };
+    
+    render() {
+        const { login } = this.state;
+        return (
+            <div className="header">
+               <img src={logo} className="svg" alt="logo" />
+
+            </div>
+        );
+    }
+}
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React, { createContext } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from "./screens/home/Home.js";
-import { BrowserRouter } from "react-router-dom";
-
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import Details from "./common/Details/Details";
+import home from "./screens/home/Home"
+import BookShow from './screens/bookshow/BookShow';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Home />
+    <Switch>
+      <Route exact path="/" component={home} />
+      {/* <Route path="/details/:id" component={Details} />
+      <Route path="/bookshow" component={BookShow} /> */}
+      </Switch>
   </BrowserRouter>
   ,
+  //hggghjgh
   document.getElementById('root')
 );
 
-
+// If you wa?/nt to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
